6bcbd5817b0a56a51ff6fd90796649c493a29c32
maintenancecp <maintenance.cpps@gmail.com>
2025-07-12 02:32:39 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASlYaxOMg36n5j6ffqYRntJ5v0lwaQSzI",
+  authDomain: "test-progarm.firebaseapp.com",
+  projectId: "test-progarm",
+  storageBucket: "test-progarm.appspot.com",
+  messagingSenderId: "967694649194",
+  appId: "1:967694649194:web:b9df00e355a27a90a7c713",
+  measurementId: "G-855F2GCTLX"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, user => {
+  document.getElementById("loginPage").style.display = user ? "none" : "block";
+  document.getElementById("appContainer").style.display = user ? "flex" : "none";
+});
+
+window.login = async function () {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    document.getElementById("loginError").textContent = err.message;
+  }
+};
+
+window.register = async function () {
+  const email = document.getElementById("loginEmail").value;
+  const password = document.getElementById("loginPassword").value;
+  try {
+    await createUserWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    document.getElementById("loginError").textContent = err.message;
+  }
+};
+
+window.logout = function () {
+  signOut(auth);
+};
