ccb00fadd27cf91cc14fac68b791d7f17e13a45b
maintenancecp <maintenance.cpps@gmail.com>
2025-07-12 03:55:46 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyASlYaxOMg36n5j6ffqYRntJ5v0lwaQSzI",
   authDomain: "test-progarm.firebaseapp.com",
   projectId: "test-progarm",
-  storageBucket: "test-progarm.appspot.com",
+  storageBucket: "test-progarm.firebasestorage.app",
   messagingSenderId: "967694649194",
   appId: "1:967694649194:web:b9df00e355a27a90a7c713",
   measurementId: "G-855F2GCTLX"
