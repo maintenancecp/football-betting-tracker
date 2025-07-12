02ad03204165fab594706a5b6cdca9974975ec4d
maintenancecp <maintenance.cpps@gmail.com>
2025-07-13 02:32:09 +0700

    Update firebase-config.js

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -1,9 +1,8 @@
-// กำหนด config firebase และ initialize app + auth
 const firebaseConfig = {
   apiKey: "AIzaSyASlYaxOMg36n5j6ffqYRntJ5v0lwaQSzI",
   authDomain: "test-progarm.firebaseapp.com",
   projectId: "test-progarm",
-  storageBucket: "test-progarm.firebasestorage.app",
+  storageBucket: "test-progarm.appspot.com",
   messagingSenderId: "967694649194",
   appId: "1:967694649194:web:b9df00e355a27a90a7c713",
   measurementId: "G-855F2GCTLX"
